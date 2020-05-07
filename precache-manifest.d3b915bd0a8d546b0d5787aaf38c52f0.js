5418c8befcec72ac0ad27f10fea15c9a34a31c4a
abadik <andrej.badik@gmail.com>
2020-05-07 18:51:23 +0200

    Updates

---
+++ precache-manifest.d3b915bd0a8d546b0d5787aaf38c52f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737d867e971ad6d621ccd18db9ac1f1f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "304e19f3b68ea95e5aa3",
+    "url": "/monster-rolodex/static/css/main.c7914dc2.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304e19f3b68ea95e5aa3",
+    "url": "/monster-rolodex/static/js/main.a7973f82.chunk.js"
+  },
+  {
+    "revision": "90506cc279e62bb14608",
+    "url": "/monster-rolodex/static/js/runtime-main.59621b72.js"
+  }
+]);
